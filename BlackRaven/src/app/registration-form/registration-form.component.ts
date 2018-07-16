124f0084ff98e5e130a2f099de2d032b93f73299
Kade H <kadeiszman@gmail.com>
2018-07-15 23:04:36 -0500

    Added registration page
    added name, password fields with validation on the password and confirm password fields

--- BlackRaven/src/app/registration-form/registration-form.component.ts
+++ BlackRaven/src/app/registration-form/registration-form.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { User } from '../shared/user';
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.css']
 })
-export class RegistrationFormComponent implements OnInit {
+export class RegistrationFormComponent {
 
-  constructor() { }
+  newUser = new User();
+  submitted = false;
 
-  ngOnInit() {
+  onSubmit() {
+    let saveSuccessful = false;
+    this.submitted = true;
+
+    if (this.validateForm()) {
+      saveSuccessful = this.saveUser(this.createNewUser());
+    }
+
+    return saveSuccessful;
   }
 
+  private validateForm () {
+    return this.newUser.password === this.newUser.confirmPassword;
+  }
+
+  private createNewUser() {
+    return new User(this.newUser.name, this.newUser.password, this.newUser.confirmPassword);
+  }
+
+  private saveUser(user: User) {
+    // todo save this user
+    console.log('User Saved: ' + JSON.stringify(user));
+    return true;
+  }
+
+  // get diagonastic() { return JSON.stringify(this.newUser); }
 }
